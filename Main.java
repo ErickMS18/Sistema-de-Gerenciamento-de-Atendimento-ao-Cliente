9d1aa9114fb823501dbcdb9a54fd1c06d4bee9df
Peko <erickms1816@gmail.com>
2025-10-24 10:14:37 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -22,16 +22,16 @@ public class Main {
         };
 
         Elemento[] listaHistorico = new Elemento[] {
-                new Elemento("REQ001", "Instalação de software", "2024-08-20 10:30"),
-                new Elemento("REQ002", "Manutenção preventiva", "2024-08-20 11:00"),
-                new Elemento("REQ003", "Atualização de sistema", "2024-08-20 11:30"),
-                new Elemento("REQ004", "Suporte técnico", "2024-08-20 12:00"),
-                new Elemento("REQ005", "Troca de equipamento", "2024-08-20 12:30"),
-                new Elemento("REQ006", "Consulta de garantia", "2024-08-20 13:00"),
-                new Elemento("REQ007", "Reparo de impressora", "2024-08-20 13:30"),
-                new Elemento("REQ008", "Configuração de rede", "2024-08-20 14:00"),
-                new Elemento("REQ009", "Restauração de dados", "2024-08-20 14:30"),
-                new Elemento("REQ010", "Consulta técnica", "2024-08-20 15:00")
+                new Elemento("REQ001", "Instalação de software", "2025-10-18 10:30"),
+                new Elemento("REQ002", "Manutenção preventiva", "2025-10-18 11:00"),
+                new Elemento("REQ003", "Atualização de sistema", "2025-10-18 11:30"),
+                new Elemento("REQ004", "Suporte técnico", "2025-10-18 12:00"),
+                new Elemento("REQ005", "Troca de equipamento", "2025-10-18 12:30"),
+                new Elemento("REQ006", "Consulta de garantia", "2025-10-18 13:00"),
+                new Elemento("REQ007", "Reparo de impressora", "2025-10-18 13:30"),
+                new Elemento("REQ008", "Configuração de rede", "2025-10-18 14:00"),
+                new Elemento("REQ009", "Restauração de dados", "2025-10-18 14:30"),
+                new Elemento("REQ010", "Consulta técnica", "2025-10-18 15:00")
         };
 
         atendimento.popularFila(listaAtendimento);
